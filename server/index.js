6c89581de7e75b079d45e9c30c25fb120453be45
Matt Upham <mcupham@gmail.com>
2018-03-07 10:27:18 -0800

    link up api key

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const path = require('path');
+const movieAPI = require('../lib/movieAPI.js');
 
 const db = [
   {
